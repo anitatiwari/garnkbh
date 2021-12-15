240ce6328ad8838d76e3326836d8adeeb612bc85
Anita Tiwari <anit8621@stud.kea.dk>
2021-12-15 10:36:09 +0100

    jsfetched

---
+++ product.js
@@ -0,0 +1,76 @@
+
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+
+const url = "https://garnkbh-492c.restdb.io/rest/garnkbh/"+id;
+const options = {
+  headers: {
+    "x-apikey": "61acfc6f72a03f5dae822131",
+  },
+};
+
+          
+
+fetch(url,options)
+
+  .then(function (res) {
+    return res.json();
+    
+  })
+  .then(function (data) {
+    productlist(data);
+  });
+  
+    function productlist(product) {
+    console.log(product);
+ document.querySelector(".price").textContent = product.price;
+document.querySelector(".productimage").src=product.image;
+  document.querySelector(".smallimg1").src=product.img2;
+document.querySelector(".smallimg2").src=product.img3;
+
+  document.querySelector(".description1").textContent = product.description1;
+
+  document.querySelector(".description2").textContent = product.description2;
+ 
+  document.querySelector(".description3").textContent = product.description3;
+  document.querySelector(".description4").textContent = product.description4;
+
+ document.querySelector(".productName").textContent=product.name;
+}
+
+// //   document.querySelector("a").href = "productpage.html?id=" + product.id;
+//   document.querySelector(".productName").textContent=product.name;
+
+//   document.querySelector(".productimage").src = product.image;
+// document.querySelector(".price").textContent = `${product.price}`;
+
+// document.querySelector(".smallimg1").src=product.img1;
+// document.querySelector(".smallimg2").src=product.img2;
+
+//   document.querySelector(".description1").textContent = product.description1;
+
+//   document.querySelector(".description2").textContent = product.description2;
+//   document.querySelector(".description3").textContent = product.description3;
+//   document.querySelector(".description4").textContent = product.description4;}
+
+
+
+
+
+
+
+
+var poke1 = document.getElementById('poke1');
+    var cmp = document.getElementById('counter');
+    poke1.addEventListener('click', myfonction1)
+    function myfonction1() {
+      cmp.innerHTML = parseInt(cmp.innerHTML) -1;}
+
+      var poke2 = document.getElementById('poke2');
+    var cmp = document.getElementById('counter');
+    poke2.addEventListener('click', myfonction)
+    function myfonction() {
+      cmp.innerHTML = parseInt(cmp.innerHTML) +1;}
---
+++ productlist.js
@@ -0,0 +1,46 @@
+const urlParams = new URLSearchParams(window.location.search);
+const collection = urlParams.get("collection");
+
+
+const url = "https://garnkbh-492c.restdb.io/rest/garnkbh?collection=" +collection;
+const options = {
+  headers: {
+    "x-apikey": "61acfc6f72a03f5dae822131",
+  },
+};
+fetch(url, options)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    // console.log(data);
+    handleProductList(data);
+  });
+
+
+function handleProductList(data) {
+
+  data.forEach(showProduct);
+}
+function showProduct( product){
+  
+        console.log(product);
+       
+        const template = document.querySelector("#template2").content;
+        const copy = template.cloneNode(true);
+
+        // copy.querySelector("h5").textcontent=`${product.brand}`;
+      
+        // copy.querySelector(".brand").textContent=product.brand;
+        // copy.querySelector("a").href = "product.html?id=" + product._id;
+        copy.querySelector("a").href +=product._id;
+
+        copy.querySelector(".productName1").textContent=product.name;
+     copy.querySelector( "img"  ).src =product.image ;
+     copy.querySelector(".price").textContent = `${product.price}`;
+    //  copy.querySelector(".brand").textContent = product.brand;
+
+        const parent = document.querySelector("main");
+        parent.appendChild(copy);
+  
+;}
