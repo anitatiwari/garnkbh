16518d8fcad675b4aec86ff7524a89cc310f6cfa
Anita Tiwari <anit8621@stud.kea.dk>
2021-12-19 11:22:44 +0100

    changes

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-
+// function openTheForm() {
+//     document.getElementById("popupForm").style.display = "block";
+//   }
+  
+//   function closeTheForm() {
+//     document.getElementById("popupForm").style.display = "none";
+//   }
+document.querySelector(".popupform").addEventListener("click",function(){
+    document.querySelector(".popupform").classList.add("active");
+})
+document.querySelector(".popupform.close_botton").addEventListener("click",function(){
+    document.querySelector(".popupform").classList.add("active");
+})
